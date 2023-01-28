b6dc83f70cea246b831bc71d9163a22a14f0213c
Daniel Gómez <51272519+devenapuros@users.noreply.github.com>
2023-01-27 20:10:00 -0600

    Add about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,20 +7,39 @@ export default function About() {
         <>
             <Topbar />
             <Section>
-                <h1>About</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Quam, in eaque blanditiis tempore officiis, labore nihil
-                    placeat adipisci voluptatibus veniam dolorem? Labore, at
-                    explicabo. Vero magni porro ad eaque quos!
-                </p>
-                <h2>Contributors</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Quam, in eaque blanditiis tempore officiis, labore nihil
-                    placeat adipisci voluptatibus veniam dolorem? Labore, at
-                    explicabo. Vero magni porro ad eaque quos!
-                </p>
+                <div>
+                    <h1>About</h1>
+                    <p>
+                        This project was created to collect and share hacking
+                        and cybersecurity tools with the S4vitar community.
+                    </p>
+                    <p>
+                        It is developed with Next.js and deployed on Vercel
+                        services.
+                    </p>
+                    <strong>
+                        Resources to learn hacking and cybersecurity:
+                    </strong>
+                    <ul>
+                        <li>
+                            S4vitar&apos;s twitch channel:{" "}
+                            <a
+                                href="https://twitch.tv/s4vitaar"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                twitch.tv/s4vitaar
+                            </a>
+                        </li>
+                        <li>
+                            Hack4u Academy:{" "}
+                            <a href="https//hack4u.io">hack4u.io</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Contributors</h3>
+                </div>
             </Section>
             <Footer />
         </>
