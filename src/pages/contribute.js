fb66f393aa661b9b44b9a26966bacf7c4aa12032
Daniel Gómez <51272519+devenapuros@users.noreply.github.com>
2023-01-30 14:21:47 -0600

    Finish contribute section

--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -1,4 +1,5 @@
 import { Footer } from "@/components/footer";
+import { PrimaryButton } from "@/components/primaryButton";
 import { Section } from "@/components/section";
 import { Topbar } from "@/components/topbar";
 import styles from "@/styles/contribute.module.css";
@@ -10,32 +11,114 @@ export default function Contribute() {
             <Section className={styles.contributeSection}>
                 <h1>Contribute</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Quam, in eaque blanditiis tempore officiis, labore nihil
-                    placeat adipisci voluptatibus veniam dolorem? Labore, at
-                    explicabo. Vero magni porro ad eaque quos!
+                    We are happy to receive your contributions to improve this
+                    website or add more cybersecurity tools and share them with
+                    the community. Before you start contributing please note the
+                    following guidelines:
                 </p>
-                <h2>Add a new tool</h2>
+                <h2>Report bugs or request a new feature</h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
-                    delectus reprehenderit iure corporis quas animi suscipit
-                    obcaecati quisquam soluta debitis natus facilis praesentium
-                    eaque alias iusto tempora sed nemo. Rem.
+                    If you want to report a bug, typo errors, wrong information
+                    or even request a new feature you can do it in the{" "}
+                    <a
+                        href="https://github.com/devenapuros/hack4tools/issues"
+                        className={styles.link}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Issues
+                    </a>{" "}
+                    section of the repository of this project.
                 </p>
-                <h2>Report bugs</h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Suscipit, dolorem unde. Neque, odit perferendis commodi
-                    eligendi, iusto ipsum autem reprehenderit eos officiis
-                    corporis, ex saepe porro quo molestiae ratione in!
+                    Try to be as detailed and concise as possible. If necessary,
+                    include screenshots or videos to give more context to your
+                    request.
                 </p>
-                <h2>Develop a new feature</h2>
+                <h2>Contribute with coding</h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Error, esse et? Quo aut repellendus soluta corporis ipsum
-                    eum, harum quas temporibus sint voluptatem, debitis
-                    reprehenderit omnis culpa ipsam eaque perferendis!
+                    Great! We love to receive code contributions, but before you
+                    start, you will need to install a couple of dependencies and
+                    follow the steps below:
                 </p>
+                <h4>Dependencies</h4>
+                <ul>
+                    <li>
+                        Node.js 18.13.0 (We recommend using{" "}
+                        <a
+                            className={styles.link}
+                            target="_blank"
+                            rel="noreferrer"
+                            href=""
+                        >
+                            NVM
+                        </a>{" "}
+                        or{" "}
+                        <a
+                            className={styles.link}
+                            target="_blank"
+                            rel="noreferrer"
+                            href=""
+                        >
+                            FNM
+                        </a>
+                        )
+                    </li>
+                    <li>Visual Studio Code Editor</li>
+                    <li>
+                        <a
+                            className={styles.link}
+                            target="_blank"
+                            rel="noreferrer"
+                            href=""
+                        >
+                            Eslint
+                        </a>{" "}
+                        VS Code Extension
+                    </li>
+                    <li>
+                        <a
+                            className={styles.link}
+                            target="_blank"
+                            rel="noreferrer"
+                            href=""
+                        >
+                            Prettier
+                        </a>{" "}
+                        VS Code Extension
+                    </li>
+                    <li>Git 2.3 or latest</li>
+                </ul>
+                <h4>Steps to contribute with coding</h4>
+                <ol>
+                    <li>
+                        Fork the{" "}
+                        <a
+                            className={styles.link}
+                            target="_blank"
+                            rel="noreferrer"
+                            href=""
+                        >
+                            project repository
+                        </a>
+                    </li>
+                    <li>
+                        Make a <code className={styles.code}>git clone</code> of
+                        the project
+                    </li>
+                    <li>
+                        Install the project dependencies with{" "}
+                        <code className={styles.code}>npm install</code>{" "}
+                    </li>
+                    <li>
+                        Make your changes and upload them to the forked repo on
+                        Github.
+                    </li>
+                    <li>
+                        Open a Pull Request in the original repository. Include
+                        a concise description of the changes you have made.
+                    </li>
+                </ol>
             </Section>
             <Footer />
         </>
