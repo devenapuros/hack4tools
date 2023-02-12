92f9c8d479f000d2c4fd5af856c1297007322dbc
Daniel Gómez <51272519+devenapuros@users.noreply.github.com>
2023-02-12 15:08:09 -0600

    Fix links on hero

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,20 +1,23 @@
+import Link from "next/link";
 import { ImageBox } from "./imageBox";
 import styles from "@/styles/carouselItem.module.css";
 
 export const CarouselItem = ({ tool, dataIndex = 0 }) => {
     return (
         <li className={styles.item} data-index={dataIndex}>
-            <ImageBox
-                size={100}
-                color={tool.color}
-                alt={tool.name}
-                corner={18}
-            />
-            <div>
-                <h1>{tool.name}</h1>
-                <small>By {tool.autor}</small>
-            </div>
-            <p>{tool.slogan || tool.description}</p>
+            <Link href={`/tool/${tool.id}`} className={styles.link}>
+                <ImageBox
+                    size={100}
+                    color={tool.color}
+                    alt={tool.name}
+                    corner={18}
+                />
+                <div>
+                    <h1>{tool.name}</h1>
+                    <small>By {tool.autor}</small>
+                </div>
+                <p>{tool.slogan || tool.description}</p>
+            </Link>
         </li>
     );
 };
