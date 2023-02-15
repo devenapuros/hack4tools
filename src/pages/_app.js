b0a80852886a8eb5bd577182297bd082779adf8a
Daniel Gómez <51272519+devenapuros@users.noreply.github.com>
2023-02-15 11:08:19 -0600

    Add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,8 +8,25 @@ export default function App({ Component, pageProps }) {
             <Head>
                 <meta
                     name="description"
-                    content="Awesome hacking and cybersecurity tools"
+                    content="The best cybersecurity and hacking tools used by pentesters around the world."
                 />
+                <meta
+                    name="keywords"
+                    content="hacking,cybersecurity,tools,s4vitar,s4vitaar,hack4u,hack4tools,omar,Omar"
+                />
+                <meta name="author" content="Devenapuros" />
+
+                <meta
+                    name="og:title"
+                    content="Hack4tools - Hacking and cybersecurity tools"
+                />
+                <meta name="og:image" content="/board.png" />
+                <meta name="og:url" content="https://hack4tools.vercel.app" />
+                <meta
+                    name="og:description"
+                    content="The best cybersecurity and hacking tools used by pentesters around the world."
+                />
+
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1"
